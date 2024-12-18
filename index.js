7bfc11591ea6536e72df97de197c9ef77c2e40ca
Muhammad-Yusuf-2766 <strangerbellimo@gmail.com>
2024-12-18 14:29:20 +0900

    dockerfile config

--- index.js
+++ index.js
@@ -24,13 +24,7 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(
 	cors({
-		origin: function (origin, callback) {
-			if (!origin || allowedOrigins.includes(origin)) {
-				callback(null, true)
-			} else {
-				callback(new Error('CORS policy violation'))
-			}
-		},
+		origin: allowedOrigins,
 		credentials: true,
 	})
 ) // Cookie'larni yuborish uchun
