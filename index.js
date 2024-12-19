8785cf185c3f3152273ce8b3de284e4ca5e76a7a
Muhammad-Yusuf-2766 <strangerbellimo@gmail.com>
2024-12-19 12:26:55 +0900

    cors origin true

--- index.js
+++ index.js
@@ -14,10 +14,10 @@ const server = http.createServer(app)
 
 // Ruxsat etilgan domenlar
 const allowedOrigins = [
-	'https://infogssiot.com',
-	'https://www.infogssiot.com',
+	'http://infogssiot.com',
+	'http://www.infogssiot.com',
 	'http://43.203.125.106:3001',
-	'http://localhost:3001',
+	// 'http://localhost:3001',
 ]
 
 app.use(express.json())
@@ -25,13 +25,13 @@ app.use(cookieParser())
 app.use(
 	cors({
 		origin: true,
-		// origin: function (origin, callback) {
-		// 	if (!origin || allowedOrigins.includes(origin)) {
-		// 		callback(null, true)
-		// 	} else {
-		// 		callback(new Error('CORS policy violation'))
-		// 	}
-		// },
+		origin: function (origin, callback) {
+			if (!origin || allowedOrigins.includes(origin)) {
+				callback(null, true)
+			} else {
+				callback(new Error('CORS policy violation'))
+			}
+		},
 		credentials: true,
 	})
 ) // Cookie'larni yuborish uchu
